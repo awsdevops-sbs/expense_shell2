c8c8ea4010f92c3114261723ff1f037e306d7f49
akram <akram.khan200@gmail.com>
2025-02-16 00:53:59 +0530

    iteration 21293

--- frontend.sh
+++ frontend.sh
@@ -6,10 +6,12 @@ print "install nginx"
 dnf install nginx -y
 check_status $?
 
-App_req
+
 print "copy the config file"
 cp expense.conf /etc/nginx/default.d/expense.conf
 check_status $?
+
+App_req
 
 print "Start the nginx service"
 systemctl enable nginx
