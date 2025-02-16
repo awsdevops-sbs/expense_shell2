135a2da9c0ad3d69f9036fa7dab683135251d670
akram <akram.khan200@gmail.com>
2025-02-16 16:40:59 +0530

    iteration 3453asdffghg

--- backend.sh
+++ backend.sh
@@ -7,45 +7,45 @@ if [ -z "$pass" ]; then
    exit 1
   fi
 
-print "Disable current module " &>>$Log
+print "Disable current module "
 dnf module disable nodejs -y
-check_status $? &>>$Log
+check_status $?
 
-print "Enable the nodejs version 20" &>>$Log
+print "Enable the nodejs version 20"
 dnf module enable nodejs:20 -y
-check_status $? &>>$Log
+check_status $?
 
-print "Installing nodejs " &>>$Log
+print "Installing nodejs "
 dnf install nodejs -y
-check_status $? &>>$Log
+check_status $?
 
-print "Adding application user " &>>$Log
+print "Adding application user "
 id expense &>>$Log
 if [ $? -eq 0 ]; then
  useradd expense &>>$Log
 fi
-check_status $? &>>$Log
+check_status $?
 
-print "Copy backend config file " &>>$Log
+print "Copy backend config file "
 cp backend.service /etc/systemd/system/backend.service &>>$Log
-check_status $? &>>$Log
+check_status $?
 
 App_req
 
-print "Installing NPM" &>>$Log
+print "Installing NPM"
 cd /app &>>$Log
 npm install &>>$Log
 check_status $? &>>$Log
 
-print "Starting backend service" &>>$Log
+print "Starting backend service"
 systemctl daemon-reload &>>$Log
 systemctl enable backend &>>$Log
 systemctl start backend &>>$Log
-check_status $? &>>$Log
+check_status $?
 
-print "Install Mysql" &>>$Log
+print "Install Mysql"
 dnf install mysql -y
-check_status $? &>>$Log
+check_status $?
 
  print "Load sechma"
 mysql -h mysql-dev.awsdevops.sbs -uroot -p${pass} < /app/schema/backend.sql &>>$Log
