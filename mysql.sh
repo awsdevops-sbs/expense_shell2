fad48413349416ca6884673014883fcebaba856c
akram <akram.khan200@gmail.com>
2025-02-16 16:36:26 +0530

    iteration 3453asdffgh

--- mysql.sh
+++ mysql.sh
@@ -8,20 +8,20 @@ if [ -z "${pass}" ]; then
 
  print "Installing MySQL Server..."
 sudo dnf install mysql-server -y &>>$Log
-check_status $? &>>$Log
+check_status $?
 
 # Start and enable MySQL service
  print "Starting and enabling MySQL..."
 sudo systemctl start mysqld &>>$Log
 sudo systemctl enable mysqld &>>$Log
-check_status $? &>>$Log
+check_status $?
 
  print "setting up the password"
 echo "Show database" | mysql -h mysql-dev.awsdevops.sbs -uroot -p"${pass}" &>>$Log
 check_status $? &>>$Log
    if [ $? -eq 0 ]; then
      mysql_secure_installation --set-root-pass "${pass}" &>>$Log
-     check_status $? &>>$Log
+     check_status $?
      fi
 
 
