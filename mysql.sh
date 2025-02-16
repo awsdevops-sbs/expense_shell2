c866fbb77a881fbb3c541bcf2efb541b5e6b7c48
akram <akram.khan200@gmail.com>
2025-02-16 16:26:41 +0530

    iteration 3453asdf

--- mysql.sh
+++ mysql.sh
@@ -1,28 +1,28 @@
 source common.sh
-
 pass=$1
 
 if [ -z "${pass}" ]; then
    echo "no password " &>>$Log
-   exit 11
+   exit 1
    fi
 
+ print "Installing MySQL Server..." &>>$Log
 
-print "Installing MySQL Server..." &>>$Log
+
 sudo dnf install mysql-server -y &>>$Log
 check_status $? &>>$Log
 
 # Start and enable MySQL service
-print "Starting and enabling MySQL..." &>>$Log
+ print "Starting and enabling MySQL..." &>>$Log
 sudo systemctl start mysqld &>>$Log
 sudo systemctl enable mysqld &>>$Log
 check_status $? &>>$Log
 
-print "setting up the password" &>>$Log
-echo "Show database" | mysql -h mysql-dev.awsdevops.sbs -uroot -p${pass} &>>$Log
+ print "setting up the password" &>>$Log
+echo "Show database" | mysql -h mysql-dev.awsdevops.sbs -uroot -p"${pass}" &>>$Log
 check_status $? &>>$Log
    if [ $? -eq 0 ]; then
-     mysql_secure_installation --set-root-pass ${pass} &>>$Log
+     mysql_secure_installation --set-root-pass "${pass}" &>>$Log
      check_status $? &>>$Log
      fi
 
